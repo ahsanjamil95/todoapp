5813e98d2178f2cd819f2aed819d0c49086558ac
Ahsan Jamil <ahsanjamil@live.com>
2020-08-06 16:10:01 +0500

    todo

--- app.js
+++ app.js
@@ -1,60 +1,62 @@
- 
-
-var list =document.getElementById("list")
-function addTodo(){
-  var todo_item =document.getElementById("todo-item");
-
-  // create li text node
-  var li=document.createElement('li');
-  li.setAttribute("class","mb-5")
-  var litext=document.createTextNode(todo_item.value);
-  li.appendChild(litext);
-
-// create delete bbutton
-  var delBtn = document.createElement('button');
-  var delText = document.createTextNode("Delete");
-  delBtn.setAttribute("class","btn");
-  delBtn.setAttribute("class","bg-light");
-  delBtn.setAttribute("class","ml-5");
-  delBtn.setAttribute("onclick","deleteItem(this)");
-  delBtn.appendChild(delText);
-
-// create edit button
-var editBtn = document.createElement('button');
-  var editText = document.createTextNode("Edit");
-  editBtn.setAttribute("class","btn");
-  editBtn.setAttribute("class","bg-light");
-  editBtn.setAttribute("class","ml-5");
-  editBtn.setAttribute("onclick","editItem(this)");
-  editBtn.appendChild(editText);
 
 
-  li.appendChild(delBtn);
-  li.appendChild(editBtn);
-  list.appendChild(li)
-  todo_item.value = "";
-  console.log(li)
+function getVal(){
+	var input = document.getElementById("input");
+	if(input.value === ""){
+		var req = document.getElementById("req");
+		req.innerHTML = "Required Field";
+		req.delay(3000).remove();
+	}else{
+	var p = document.createElement("p");
+	p.setAttribute("class","pgf");
+	var text = document.createTextNode(input.value);
+	p.appendChild(text);
+	var div = document.getElementById("main");
+	div.appendChild(p);
+	
+	// delete btn
+	var del_btn = document.createElement("button");
+	var del_text = document.createTextNode("Delete");
+		del_btn.setAttribute("onclick","remove(this)");
+	del_btn.setAttribute("class","del");
+	del_btn.appendChild(del_text);
+	
+	// update btn
+	var updt_btn = document.createElement("button");
+	var updt_text = document.createTextNode("Update");
+	updt_btn.setAttribute("onclick","updt(this)");
+	updt_btn.setAttribute("class","updt");
+	updt_btn.appendChild(updt_text);
+	
+	// append del+ update btn
+	p.appendChild(del_btn);
+	p.appendChild(updt_btn);
+	
+	
+	input.value = "";
+	
+}	
+}
+
+// delete one item
+function remove(e){
+	e.parentNode.remove();
+}
+// delete all function
+function dellAll(){
+	main.innerHTML ="";
+}
+// update one item
+function updt(e){
+	var userInput = prompt("New Value Enter",e.parentNode.firstChild.nodeValue);
+	e.parentNode.firstChild.nodeValue = userInput;
+	
+	
 }
 
 
 
 
-// delete function
-
-function deleteItem(d){
-    d.parentNode.remove()
-}
 
 
-// deleteall function
 
-function deleteAll(){
-  list.innerHTML = "";
-}
-
-// edititem function
-
-function editItem(i){
-  var val= prompt("new value", i.parentNode.firstChild.nodeValue);
-  i.parentNode.firstChild.nodeValue=val;
-}
